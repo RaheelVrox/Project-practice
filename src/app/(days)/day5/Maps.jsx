48100e122d2a8041b06b737faec4c00b0e763270
raheel <raheel@vrox.co.uk>
2024-05-20 18:03:36 +0500

    map

--- src/app/(days)/day5/Maps.jsx
+++ src/app/(days)/day5/Maps.jsx
@@ -1,14 +1,45 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import { Link, Stack } from "expo-router";
+im;
 const Maps = () => {
   return (
-    <View>
-      <Text>Maps</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ headerShown: false, title: "" }} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="1 bd apertment"
+          description="Hello websols"
+        />
+        <Marker
+          coordinate={{ latitude: 37.78125, longitude: -122.4424 }}
+          title="2 bd apertment"
+          description="Hello websols"
+        />
+      </MapView>
     </View>
   );
 };
 
 export default Maps;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
